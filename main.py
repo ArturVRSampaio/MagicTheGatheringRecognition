f5fc5c0968b9d5419f3c2470ba5d366e197a202f
ArturVRSampaio <artur.vitorio@astisoftware.com.br>
2023-03-15 23:21:01 -0300

    rm strange symbols from image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import pytesseract
+import re
 
 framewidth = 1920
 frameheight = 1080
@@ -14,19 +15,30 @@ while True:
         break
 
     success, img = cap.read()
-
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    #remove strange simbols from the reading
+    img = cv2.rectangle(img, (625, 350), (750, 400), (0,0,0), -1)
+    img = cv2.rectangle(img, (650, 400), (670, 450), (0,0,0), -1)
+    img = cv2.rectangle(img, (760, 400), (830, 450), (0,0,0), -1)
 
     # collumn / ROW
     img = img[350:450, 530:830]
 
+    #blur + b&w image + invert
     img = cv2.blur(img, [2, 2], 0)
     img = cv2.threshold(img, 16, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
     img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel, iterations=1)
 
     if cv2.waitKey(1) & 0xFF == ord('f'):
-        text = pytesseract.image_to_string(img, config='--psm 6')
-        print(text)
+        rawText = pytesseract.image_to_string(img, config='--psm 6')
+
+        items = re.findall(r'\b\w+\b', rawText)
+
+
+        print(rawText)
+        print(items)
+        print("*******")
 
     cv2.imshow("video", img)
